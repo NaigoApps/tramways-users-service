39c159c048f6030673cbfb3bddffb8d73bb06330
naigo <lorenzo.niccolai94@gmail.com>
2020-08-21 12:51:52 +0200

    First commit

---
+++ src/main/java/it/tramways/user/UsersServiceApplication.java
@@ -0,0 +1,17 @@
+package it.tramways.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class UsersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersServiceApplication.class, args);
+    }
+
+}
