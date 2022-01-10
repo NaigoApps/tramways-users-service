97686bc10a0fbb696094bd35ef6eb921d6026e95
Lorenzo <lorenzo.niccolai94@gmail.com>
2022-01-11 00:54:28 +0100

    projects settings

--- users-service-jpa-mappers/target/generated-sources/annotations/it/tramways/users/jpa/mappers/UserMapperImpl.java
+++ users-service-jpa-mappers/target/generated-sources/annotations/it/tramways/users/jpa/mappers/UserMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-09-16T09:24:37+0200",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.11 (Ubuntu)"
+    date = "2022-01-11T00:50:35+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.12 (Amazon.com Inc.)"
 )
 @Component
 public class UserMapperImpl implements UserMapper {
